5b8d11de49d7fc4d1898a3cbac35eb1d53815bf3
nakjasabal <nakjasabal@naver.com>
2023-12-11 15:02:47 +0900

    .

---
+++ src/test/java/com/edu/springboot/AtelierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtelierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
